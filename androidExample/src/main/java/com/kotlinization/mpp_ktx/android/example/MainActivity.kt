93b98d6aeb0dd86359cf0ffb191a4db9ea7210f0
Milan Jovic <milanjovic92@live.com>
2023-07-29 17:29:25 +0200

    Fixed databinding package names.

--- androidExample/src/main/java/com/kotlinization/mpp_ktx/android/example/MainActivity.kt
+++ androidExample/src/main/java/com/kotlinization/mpp_ktx/android/example/MainActivity.kt
@@ -3,7 +3,7 @@ package com.kotlinization.mpp_ktx.android.example
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.github.kotlinizer.android.example.databinding.ActivityMainBinding
+import com.kotlinization.mpp_ktx.android.example.databinding.ActivityMainBinding
 import com.kotlinization.mpp_ktx.android.example.service.BoundServiceActivity
 
 
